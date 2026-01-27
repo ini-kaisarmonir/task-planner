3217efe7cb2e2343f12773a63721b4939ead803d
Kaisar <ini.kaisarmonir@gmail.com>
2026-01-27 08:49:29 +0600

    gate, policy, migration

---
+++ app/Enums/Priority.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum Priority: int {
+    case LOW = 1;
+    case MEDIUM = 2;
+    case HIGH = 3;
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Enums\Role;
 
 class User extends Authenticatable
 {
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -43,5 +45,25 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
         ];
+    }
+
+    public function isAdmin()
+    {
+        return $this->role === Role::ADMIN;
+    }
+
+    public function isEmployee()
+    {
+        return $this->role === Role::EMPLOYEE;
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'user_id');
+    }
+
+    public function events()
+    {
+        return $this->hasMany(Event::class, 'user_id');
     }
 }
